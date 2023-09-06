0990203474dbaa3d8a26a03438560ba46e368263
recaidonmez <141131261+recaidonmez@users.noreply.github.com>
2023-09-06 11:54:03 +0300

    Add files via upload

---
+++ DENEME.java
@@ -0,0 +1,13 @@
+package week_2_Assigments;
+
+import java.util.Scanner;
+
+public class DENEME {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        double yarıçap = input.nextDouble();
+
+
+    }
+}
